b0a47becf5bf78eddce3603a506df15dba2ad65d
Regaudie Emmanuel <regaudie4@mmi-dev.unilim.fr>
2025-04-03 09:29:37 +0200

    update credentials

--- server/model.php
+++ server/model.php
@@ -14,6 +14,6 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "mora");
-define("DBLOGIN", "root");
-define("DBPWD", "root");
+define("DBNAME", "regaudie4");
+define("DBLOGIN", "regaudie4");
+define("DBPWD", "regaudie4");
